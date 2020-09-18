3e93788be9256d0918eda91d2f51cea7051a142f
William Huster <william@thinknimble.com>
2020-09-17 21:31:16 -0400

    Add getters to Pagination

--- src/collections/pagination.js
+++ src/collections/pagination.js
@@ -17,6 +17,26 @@ const Pagination = {
     }
     return Math.ceil(totalCount / size)
   },
+  setNextPage() {
+    if (this.page === this.calcTotalPages) return
+    this.page++
+  },
+  setPrevPage() {
+    if (this.page === 1) return
+    this.page--
+  },
+  get hasPrevPage() {
+    return this.page > 1
+  },
+  get hasNextPage() {
+    return this.calcTotalPages(this) && this.page !== this.calcTotalPages(this)
+  },
+  get currentPageStart() {
+    return this.page > 1 ? (this.page - 1) * this.size : 1
+  },
+  get currentPageEnd() {
+    return Math.min(this.page > 1 ? this.page * this.size : this.size, this.totalCount)
+  },
 }
 
 export default Pagination
