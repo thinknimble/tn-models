ac2d4b85eea717e5ea358fc01bb34a6abe8900e7
William Huster <william@thinknimble.com>
2020-11-03 14:41:46 -0500

    Convert Pagination object to a class

--- src/collections/pagination.js
+++ src/collections/pagination.js
@@ -6,43 +6,51 @@ const PaginationDefaults = {
   size: 25,
 }
 
-const Pagination = {
+export default class Pagination {
+
   create(opts = {}) {
     return Object.assign(Object.create(Pagination), PaginationDefaults, { ...opts })
-  },
+  }
+
   copy() {
     return Object.assign(Object.create(Pagination), PaginationDefaults, this)
-  },
+  }
+
   update(data = {}) {
     return Object.assign(this.copy(), data)
-  },
+  }
+
   calcTotalPages(pagination) {
     const { totalCount, size } = pagination
     if (!totalCount) {
       return null
     }
     return Math.ceil(totalCount / size)
-  },
+  }
+
   setNextPage() {
     if (this.page === this.calcTotalPages) return
     this.page++
-  },
+  }
+
   setPrevPage() {
     if (this.page === 1) return
     this.page--
-  },
+  }
+
   get hasPrevPage() {
     return this.page > 1
-  },
+  }
+
   get hasNextPage() {
     return this.calcTotalPages(this) && this.page !== this.calcTotalPages(this)
-  },
+  }
+
   get currentPageStart() {
     return this.page > 1 ? (this.page - 1) * this.size : 1
-  },
+  }
+
   get currentPageEnd() {
     return Math.min(this.page > 1 ? this.page * this.size : this.size, this.totalCount)
-  },
+  }
 }
-
-export default Pagination
