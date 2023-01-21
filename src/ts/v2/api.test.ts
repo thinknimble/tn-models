3475568a131653339fb79e9cd25d623a5fd97ab0
lakardion <lakardion@protonmail.com>
2023-01-21 19:39:29 -0300

    test(v2) api.retrieve

--- src/ts/v2/api.test.ts
+++ src/ts/v2/api.test.ts
@@ -33,12 +33,12 @@ describe("v2 api tests", async () => {
       },
     },
   })
-  beforeEach(() => {
-    mockedAxios.get.mockReset()
-    mockedAxios.post.mockReset()
-  })
 
   describe("create", () => {
+    beforeEach(() => {
+      mockedAxios.post.mockReset()
+    })
+
     const createInput: GetZodInferredTypeFromRaw<typeof createZodShape> = {
       age: 19,
       lastName: "Doe",
@@ -52,8 +52,8 @@ describe("v2 api tests", async () => {
       id: randomId,
     }
 
-    it("calls api with snake case", async () => {
-      // arrange
+    it("calls api with snake_case", async () => {
+      //arrange
       const postSpy = vi.spyOn(mockedAxios, "post")
       mockedAxios.post.mockResolvedValueOnce({ data: createResponse })
       //assess
@@ -65,13 +65,42 @@ describe("v2 api tests", async () => {
         first_name: createInput.firstName,
       })
     })
-    it("returns camelCased responses", async () => {
-      // arrange
+    it("returns camelCased response", async () => {
+      //arrange
       mockedAxios.post.mockResolvedValueOnce({ data: createResponse })
       //assess
       const response = await testApi.create(createInput)
       //assert
       expect(response).toEqual({ ...createInput, id: randomId })
+    })
+  })
+
+  describe("retrieve", () => {
+    beforeEach(() => {
+      mockedAxios.post.mockReset()
+    })
+
+    it("returns camelCased entity", async () => {
+      //arrange
+      const randomUuid = uuid()
+      const entityResponse: SnakeCasedPropertiesDeep<GetZodInferredTypeFromRaw<typeof entityZodShape>> = {
+        age: 18,
+        first_name: "John",
+        last_name: "Doe",
+        id: randomUuid,
+      }
+      mockedAxios.get.mockResolvedValue({ data: entityResponse })
+      const getSpy = vi.spyOn(mockedAxios, "get")
+      //assess
+      const response = await testApi.retrieve(randomUuid)
+      //assert
+      expect(getSpy).toHaveBeenCalledWith(`users/${randomUuid}`)
+      expect(response).toEqual({
+        age: entityResponse.age,
+        firstName: entityResponse.first_name,
+        lastName: entityResponse.last_name,
+        id: randomUuid,
+      })
     })
   })
 })
