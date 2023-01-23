1d0674c14e87b6783e5d48ee3febe4952af410c0
lakardion <lakardion@protonmail.com>
2023-01-23 20:57:05 -0300

    chore: build latest

--- lib/esm/types/ts/v2/api.d.ts
+++ lib/esm/types/ts/v2/api.d.ts
@@ -1,11 +1,12 @@
-import { CamelCasedPropertiesDeep } from "@thinknimble/tn-utils";
+import { SnakeCase, SnakeCasedPropertiesDeep } from "@thinknimble/tn-utils";
 import { AxiosInstance } from "axios";
-import { z, ZodRawShape } from "zod";
+import { z, ZodRawShape, ZodTypeAny } from "zod";
+import { IPagination } from "../pagination";
 import { getPaginatedZod } from "./pagination";
 declare const paginationFiltersZod: z.ZodOptional<z.ZodObject<{
     page: z.ZodOptional<z.ZodNumber>;
     pageSize: z.ZodOptional<z.ZodNumber>;
-}, "strict", z.ZodTypeAny, {
+}, "strip", z.ZodTypeAny, {
     page?: number | undefined;
     pageSize?: number | undefined;
 }, {
@@ -15,15 +16,50 @@ declare const paginationFiltersZod: z.ZodOptional<z.ZodObject<{
 export declare type PaginationFilters = z.infer<typeof paginationFiltersZod>;
 declare const filtersZod: z.ZodOptional<z.ZodObject<{
     ordering: z.ZodOptional<z.ZodString>;
-}, "strict", z.ZodTypeAny, {
+}, "strip", z.ZodTypeAny, {
     ordering?: string | undefined;
 }, {
     ordering?: string | undefined;
 }>>;
-export declare type CustomServiceCall = (inputs: any) => Promise<unknown>;
-declare type ExtractCamelCaseValue<T extends object> = T extends undefined ? never : {
-    [TKey in keyof T]: T[TKey] extends () => Promise<infer TResult> ? () => Promise<CamelCasedPropertiesDeep<TResult>> : T[TKey] extends (input: infer TInput) => Promise<infer TResult> ? (input: TInput) => Promise<CamelCasedPropertiesDeep<TResult>> : never;
+declare type CustomServiceCallOpts<TInput extends ZodRawShape | ZodTypeAny, TOutput extends ZodRawShape | ZodTypeAny> = {
+    inputShape: TInput;
+    outputShape: TOutput;
+    callback: (params: {
+        client: AxiosInstance;
+        input: TInput extends ZodRawShape ? GetZodInferredTypeFromRaw<TInput> : TInput extends ZodTypeAny ? z.infer<TInput> : never;
+        utils: {
+            fromApi: (obj: object) => TOutput extends ZodRawShape ? GetZodInferredTypeFromRaw<TOutput> : TOutput extends ZodTypeAny ? z.infer<TOutput> : never;
+            toApi: (obj: object) => TInput extends ZodRawShape ? SnakeCasedPropertiesDeep<GetZodInferredTypeFromRaw<TInput>> : TInput extends ZodTypeAny ? z.infer<TInput> : never;
+        };
+    }) => Promise<TOutput extends ZodRawShape ? GetZodInferredTypeFromRaw<TOutput> : TOutput extends ZodTypeAny ? z.infer<TOutput> : never>;
 };
+declare type ZodPrimitives = z.ZodString | z.ZodNumber | z.ZodDate | z.ZodBigInt | z.ZodBoolean;
+/**
+ * Use this method to get the right type inference when creating a customApiCall
+ */
+export declare const createCustomServiceCall: <TInput extends z.ZodRawShape | ZodPrimitives, TOutput extends z.ZodRawShape | ZodPrimitives>(opts: CustomServiceCallOpts<TInput, TOutput>) => CustomServiceCallOpts<TInput, TOutput>;
+declare type CustomServiceCall<TOpts extends object> = TOpts extends Record<string, CustomServiceCallOpts<any, any>> ? {
+    [TKey in keyof TOpts]: (inputs: TOpts[TKey]["inputShape"] extends ZodRawShape ? GetZodInferredTypeFromRaw<TOpts[TKey]["inputShape"]> : TOpts[TKey]["inputShape"] extends ZodTypeAny ? z.infer<TOpts[TKey]["inputShape"]> : never) => Promise<TOpts[TKey]["outputShape"] extends ZodRawShape ? GetZodInferredTypeFromRaw<TOpts[TKey]["outputShape"]> : TOpts[TKey]["outputShape"] extends ZodTypeAny ? z.infer<TOpts[TKey]["outputShape"]> : never>;
+} : never;
+declare type ZodRawShapeSnakeCased<T extends ZodRawShape> = {
+    [TKey in keyof T as SnakeCase<TKey>]: T[TKey];
+};
+export declare const getPaginatedSnakeCasedZod: <T extends z.ZodRawShape>(zodShape: T) => z.ZodObject<{
+    count: z.ZodNumber;
+    next: z.ZodNullable<z.ZodString>;
+    previous: z.ZodNullable<z.ZodString>;
+    results: z.ZodArray<z.ZodObject<ZodRawShapeSnakeCased<T>, "strip", z.ZodTypeAny, z.objectUtil.addQuestionMarks<ZodRawShapeSnakeCased<T> extends infer T_3 extends z.ZodRawShape ? { [k_2 in keyof T_3]: ZodRawShapeSnakeCased<T>[k_2]["_output"]; } : never> extends infer T_1 ? { [k_1 in keyof T_1]: z.objectUtil.addQuestionMarks<ZodRawShapeSnakeCased<T> extends infer T_2 extends z.ZodRawShape ? { [k in keyof T_2]: ZodRawShapeSnakeCased<T>[k]["_output"]; } : never>[k_1]; } : never, z.objectUtil.addQuestionMarks<ZodRawShapeSnakeCased<T> extends infer T_6 extends z.ZodRawShape ? { [k_2_1 in keyof T_6]: ZodRawShapeSnakeCased<T>[k_2_1]["_input"]; } : never> extends infer T_4 ? { [k_3 in keyof T_4]: z.objectUtil.addQuestionMarks<ZodRawShapeSnakeCased<T> extends infer T_5 extends z.ZodRawShape ? { [k_2 in keyof T_5]: ZodRawShapeSnakeCased<T>[k_2]["_input"]; } : never>[k_3]; } : never>, "many">;
+}, "strip", z.ZodTypeAny, {
+    next: string | null;
+    previous: string | null;
+    count: number;
+    results: (z.objectUtil.addQuestionMarks<ZodRawShapeSnakeCased<T> extends infer T_3 extends z.ZodRawShape ? { [k_2 in keyof T_3]: ZodRawShapeSnakeCased<T>[k_2]["_output"]; } : never> extends infer T_1 ? { [k_1 in keyof T_1]: z.objectUtil.addQuestionMarks<ZodRawShapeSnakeCased<T> extends infer T_2 extends z.ZodRawShape ? { [k in keyof T_2]: ZodRawShapeSnakeCased<T>[k]["_output"]; } : never>[k_1]; } : never)[];
+}, {
+    next: string | null;
+    previous: string | null;
+    count: number;
+    results: (z.objectUtil.addQuestionMarks<ZodRawShapeSnakeCased<T> extends infer T_6 extends z.ZodRawShape ? { [k_2_1 in keyof T_6]: ZodRawShapeSnakeCased<T>[k_2_1]["_input"]; } : never> extends infer T_4 ? { [k_3 in keyof T_4]: z.objectUtil.addQuestionMarks<ZodRawShapeSnakeCased<T> extends infer T_5 extends z.ZodRawShape ? { [k_2 in keyof T_5]: ZodRawShapeSnakeCased<T>[k_2]["_input"]; } : never>[k_3]; } : never)[];
+}>;
 export declare type GetZodInferredTypeFromRaw<T extends ZodRawShape> = z.infer<ReturnType<typeof z.object<T>>>;
 declare type BareApiService<TEntity extends ZodRawShape, TCreate extends ZodRawShape, TExtraFilters extends ZodRawShape = never> = {
     client: AxiosInstance;
@@ -31,11 +67,11 @@ declare type BareApiService<TEntity extends ZodRawShape, TCreate extends ZodRawS
     create(inputs: GetZodInferredTypeFromRaw<TCreate>): Promise<GetZodInferredTypeFromRaw<TEntity>>;
     list(params?: {
         filters?: GetZodInferredTypeFromRaw<TExtraFilters> & z.infer<typeof filtersZod>;
-        pagination?: z.infer<typeof paginationFiltersZod>;
+        pagination?: IPagination;
     }): Promise<z.infer<ReturnType<typeof getPaginatedZod<TEntity>>>>;
 };
-declare type ApiService<TEntity extends ZodRawShape, TCreate extends ZodRawShape, TCustomEndpoints extends object, TExtraFilters extends ZodRawShape = never> = BareApiService<TEntity, TCreate, TExtraFilters> & {
-    customEndpoints: ExtractCamelCaseValue<TCustomEndpoints>;
+declare type ApiService<TEntity extends ZodRawShape, TCreate extends ZodRawShape, TCustomServiceCalls extends object, TExtraFilters extends ZodRawShape = never> = BareApiService<TEntity, TCreate, TExtraFilters> & {
+    customServiceCalls: CustomServiceCall<TCustomServiceCalls>;
 };
 declare type ApiBaseParams<TApiEntity extends ZodRawShape, TApiCreate extends ZodRawShape, TApiUpdate extends ZodRawShape, TExtraFilters extends ZodRawShape = never> = {
     /**
@@ -78,12 +114,11 @@ declare type ApiBaseParams<TApiEntity extends ZodRawShape, TApiCreate extends Zo
      */
     client: AxiosInstance;
 };
-export declare function createApi<TApiEntity extends ZodRawShape, TApiCreate extends ZodRawShape, TApiUpdate extends ZodRawShape, TCustomEndpoints extends Record<string, CustomServiceCall>, TExtraFilters extends ZodRawShape = never>(base: ApiBaseParams<TApiEntity, TApiCreate, TApiUpdate, TExtraFilters>, 
+export declare function createApi<TApiEntity extends ZodRawShape, TApiCreate extends ZodRawShape, TApiUpdate extends ZodRawShape, TCustomServiceCalls extends Record<string, CustomServiceCallOpts<any, any>>, TExtraFilters extends ZodRawShape = never>(base: ApiBaseParams<TApiEntity, TApiCreate, TApiUpdate, TExtraFilters>, 
 /**
- * Create your own custom endpoints to use with this API. We take care of camel and snake casing on the way in and out.
- * You will need to handle errors and reuse the same client passed in previous parameter as well as append the endpoint.
+ * Create your own custom service calls to use with this API. Tools for case conversion are provided.
  */
-customEndpoints: TCustomEndpoints): ApiService<TApiEntity, TApiCreate, TCustomEndpoints, TExtraFilters>;
+customServiceCalls: TCustomServiceCalls): ApiService<TApiEntity, TApiCreate, TCustomServiceCalls, TExtraFilters>;
 export declare function createApi<TApiEntity extends ZodRawShape, TApiCreate extends ZodRawShape, TApiUpdate extends ZodRawShape, TExtraFilters extends ZodRawShape = never>(base: ApiBaseParams<TApiEntity, TApiCreate, TApiUpdate, TExtraFilters>): BareApiService<TApiEntity, TApiCreate, TExtraFilters>;
 export {};
 //# sourceMappingURL=api.d.ts.map
--- lib/esm/types/ts/v2/collection-manager.d.ts
+++ lib/esm/types/ts/v2/collection-manager.d.ts
@@ -12,22 +12,24 @@ declare type FilterFn<TFilter, TEntity> = (params?: {
     pagination?: IPagination;
 }) => Promise<PaginationResult<TEntity>>;
 declare type FilterParam<T extends FilterFn<any, any>> = T extends FilterFn<infer TFilters, any> ? TFilters : never;
-export declare const createCollectionManager: <TFetchList extends FilterFn<any, any>, TEntity extends ZodRawShape>({ fetchList, list: feedList, filters, pagination: feedPagination, entityZodShape, refreshing: feedRefreshing, loadingNextPage: feedLoadingNextPage, }: {
+export declare const createCollectionManager: <TFetchList extends FilterFn<any, any>, TEntityZodShape extends ZodRawShape>({ fetchList, list: feedList, filters, pagination: feedPagination, entityZodShape, refreshing: feedRefreshing, loadingNextPage: feedLoadingNextPage, }: {
     fetchList: TFetchList;
-    list?: (import("zod").objectUtil.addQuestionMarks<{ [k_2 in keyof TEntity]: TEntity[k_2]["_output"]; }> extends infer T ? { [k_1 in keyof T]: import("zod").objectUtil.addQuestionMarks<{ [k in keyof TEntity]: TEntity[k]["_output"]; }>[k_1]; } : never)[] | undefined;
-    entityZodShape: TEntity;
+    entityZodShape: TEntityZodShape;
+    list?: (import("zod").objectUtil.addQuestionMarks<{ [k_2 in keyof TEntityZodShape]: TEntityZodShape[k_2]["_output"]; }> extends infer T ? { [k_1 in keyof T]: import("zod").objectUtil.addQuestionMarks<{ [k in keyof TEntityZodShape]: TEntityZodShape[k]["_output"]; }>[k_1]; } : never)[] | undefined;
     refreshing?: boolean | undefined;
     loadingNextPage?: boolean | undefined;
     filters?: FilterParam<TFetchList> | undefined;
     pagination?: IPagination | undefined;
 }) => {
-    update: (data: PaginationResult<import("zod").objectUtil.addQuestionMarks<{ [k_2 in keyof TEntity]: TEntity[k_2]["_output"]; }> extends infer T ? { [k_1 in keyof T]: import("zod").objectUtil.addQuestionMarks<{ [k in keyof TEntity]: TEntity[k]["_output"]; }>[k_1]; } : never>, append?: boolean) => void;
+    update: (data: PaginationResult<import("zod").objectUtil.addQuestionMarks<{ [k_2 in keyof TEntityZodShape]: TEntityZodShape[k_2]["_output"]; }> extends infer T ? { [k_1 in keyof T]: import("zod").objectUtil.addQuestionMarks<{ [k in keyof TEntityZodShape]: TEntityZodShape[k]["_output"]; }>[k_1]; } : never>, append?: boolean) => void;
     refresh: () => Promise<void>;
-    nextPage: () => void;
-    prevPage: () => void;
+    nextPage: () => Promise<void>;
+    prevPage: () => Promise<void>;
     addNextPage: () => Promise<void>;
+    readonly list: (import("zod").objectUtil.addQuestionMarks<{ [k_2 in keyof TEntityZodShape]: TEntityZodShape[k_2]["_output"]; }> extends infer T ? { [k_1 in keyof T]: import("zod").objectUtil.addQuestionMarks<{ [k in keyof TEntityZodShape]: TEntityZodShape[k]["_output"]; }>[k_1]; } : never)[];
     refreshing: boolean;
     loadingNextPage: boolean;
+    readonly pagination: IPagination;
 };
 export {};
 //# sourceMappingURL=collection-manager.d.ts.map
