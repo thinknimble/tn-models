fbdb76ef3ca232ef5de1ef616ea716f27c688023
lakardion <lakardion@protonmail.com>
2023-01-24 12:15:41 -0300

    fix: add missing export of createCustomServiceCall

--- lib/esm/types/ts/v2/index.d.ts
+++ lib/esm/types/ts/v2/index.d.ts
@@ -1,4 +1,4 @@
-export { createApi } from "./api";
+export { createApi, createCustomServiceCall } from "./api";
 export { createCollectionManager } from "./collection-manager";
 export { parseResponse } from "./utils";
 export type { GetZodInferredTypeFromRaw } from "./api";
